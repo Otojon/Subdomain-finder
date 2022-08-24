2ebf23818bc0dbf3fe09be19aa1a4934e5ae7582
0x0t4jon <96408944+OtojonKhudayarov@users.noreply.github.com>
2022-08-24 13:40:15 +0500

    Add files via upload

---
+++ READme.md
@@ -0,0 +1,9 @@
+It helps you to find subdomains for target website.
+First you should install "requests" library from python to run it => $ pip install requests
+
+To run program both python file (subdomain_finder.py) and text file (subdomains.txt) should the same folder 
+
+
+In Linux you can make executable hereby attached python file => 
+$ chmod +x subdomain_finder.py  then run it,
+$./subdomain_finder.py
---
+++ subdomain_finder.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+#v1.0
+
+
+import requests
+print(  "  ______   _______   __    __  ______ ____",  
+ "\n /      \ |\      \ |\ \  |\ \|\     \    \ ", 
+"\n|  $$$$$$\| $$$$$$$\| $$  | $$| $$$$$$\$$$$ ",
+"\n| $$    $$| $$  | $$| $$  | $$| $$ | $$ | $$",
+"\n| $$$$$$$$| $$  | $$| $$__/ $$| $$ | $$ | $$",
+ " \n \$$     \| $$  | $$ \$$    $$| $$ | $$ | $$",
+  "\n  \$$$$$$$ \$$   \$$  \$$$$$$  \$$  \$$  \$$"
+  ,"by @otojonxudayarov"
+                                            )
+#ask domain name
+domain=str(input("enter domain name(for example => Google.com): "))
+#use  valid text file to enumerate
+file=open("subdomains.txt")
+sub=file.read()
+#split the new lines
+subdomains=sub.splitlines()
+founded_domains=[]
+print("please wait...")
+for subdomain in subdomains:
+    #build the url :)
+    url=f"http://{subdomain}.{domain}"
+    try:
+        requests.get(url)
+    except requests.ConnectionError:
+        #if subdomain doesn't exist it just passes
+        pass
+    else:
+        print("[+]found :", url)
+        #append discoveries to created list 
+        founded_domains.append(url)
